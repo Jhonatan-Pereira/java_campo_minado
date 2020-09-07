83449046834f1136b37922c467b3ad908ba4e479
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-09-06 23:28:53 -0300

    Initial commit

---
+++ src/br/com/jhonatan/cm/excecao/ExplosaoException.java
@@ -0,0 +1,7 @@
+package br.com.jhonatan.cm.excecao;
+
+public class ExplosaoException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ test/br/com/jhonatan/cm/modelo/CampoTeste.java
@@ -0,0 +1,58 @@
+package br.com.jhonatan.cm.modelo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CampoTeste {
+	
+	private Campo campo;
+	
+	@BeforeEach
+	void iniciarCampo() {
+		campo = new Campo(3, 3);
+	}
+
+	@Test
+	void testeVizinhoDistanciaEsquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaDireita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaEmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaEmBaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistancia2() {
+		Campo vizinho = new Campo(2, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+}
