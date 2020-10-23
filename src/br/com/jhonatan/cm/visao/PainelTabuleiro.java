c0dabd33d0d0d1b6a0283e44f531543327def19b
Jhonatan Frade <jhonatanfrade.pereira@gmail.com>
2020-10-23 20:21:15 -0300

    inicio da interface grafica com JFrame

---
+++ src/br/com/jhonatan/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,14 @@
+package br.com.jhonatan.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.jhonatan.cm.modelo.Tabuleiro;
+
+@SuppressWarnings("serial")
+public class PainelTabuleiro extends JPanel {
+
+  public PainelTabuleiro(Tabuleiro tabuleiro) {
+    
+  }
+
+}
